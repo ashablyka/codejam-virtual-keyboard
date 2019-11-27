57798c349ca2af54ec3a08f82a9c9a7678005504
Andrei Shablyka <an.shablyka@gmail.com>
2019-11-27 06:36:59 +0300

    refactor: set attributes for buttons in html

--- script.js
+++ script.js
@@ -60,83 +60,83 @@ keyboard.children[4].children[7].classList.add('arrow');
 
 const buttons = [
     [
-        { code: 192, en: '`', ru: 'ё', printable: true },
-        { code: 49, en: '1', ru: '1', printable: true },
-        { code: 50, en: '2', ru: '2', printable: true },
-        { code: 51, en: '3', ru: '3', printable: true },
-        { code: 52, en: '4', ru: '4', printable: true },
-        { code: 53, en: '5', ru: '5', printable: true },
-        { code: 54, en: '6', ru: '6', printable: true },
-        { code: 55, en: '7', ru: '7', printable: true },
-        { code: 56, en: '8', ru: '8', printable: true },
-        { code: 57, en: '9', ru: '9', printable: true },
-        { code: 48, en: '0', ru: '0', printable: true },
-        { code: 189, en: '-', ru: '-', printable: true },
-        { code: 187, en: '=', ru: '=', printable: true },
-        { code: 8, en: 'Backspace', ru: 'Backspace', printable: false }
+        { code: 'Backquote', en: '`', ru: 'ё', printable: true },
+        { code: 'Digit1', en: '1', ru: '1', printable: true },
+        { code: 'Digit2', en: '2', ru: '2', printable: true },
+        { code: 'Digit3', en: '3', ru: '3', printable: true },
+        { code: 'Digit4', en: '4', ru: '4', printable: true },
+        { code: 'Digit5', en: '5', ru: '5', printable: true },
+        { code: 'Digit6', en: '6', ru: '6', printable: true },
+        { code: 'Digit7', en: '7', ru: '7', printable: true },
+        { code: 'Digit8', en: '8', ru: '8', printable: true },
+        { code: 'Digit9', en: '9', ru: '9', printable: true },
+        { code: 'Digit0', en: '0', ru: '0', printable: true },
+        { code: 'Minus', en: '-', ru: '-', printable: true },
+        { code: 'Equal', en: '=', ru: '=', printable: true },
+        { code: 'Backspace', en: 'Backspace', ru: 'Backspace', printable: false }
     ],
 
     [
-        { code: 9, en: 'Tab', ru: 'Tab', printable: false },
-        { code: 81, en: 'q', ru: 'й', printable: true },
-        { code: 87, en: 'w', ru: 'ц', printable: true },
-        { code: 69, en: 'e', ru: 'у', printable: true },
-        { code: 82, en: 'r', ru: 'к', printable: true },
-        { code: 84, en: 't', ru: 'е', printable: true },
-        { code: 89, en: 'y', ru: 'н', printable: true },
-        { code: 85, en: 'u', ru: 'г', printable: true },
-        { code: 73, en: 'i', ru: 'ш', printable: true },
-        { code: 79, en: 'o', ru: 'щ', printable: true },
-        { code: 80, en: 'p', ru: 'з', printable: true },
-        { code: 219, en: '[', ru: 'х', printable: true },
-        { code: 221, en: ']', ru: 'ъ', printable: true },
-        { code: 220, en: '\\', ru: '\\', printable: true },
-        { code: 46, en: 'DEL', ru: 'DEL', printable: false },
+        { code: 'Tab', en: 'Tab', ru: 'Tab', printable: false },
+        { code: 'KeyQ', en: 'q', ru: 'й', printable: true },
+        { code: 'KeyW', en: 'w', ru: 'ц', printable: true },
+        { code: 'KeyE', en: 'e', ru: 'у', printable: true },
+        { code: 'KeyR', en: 'r', ru: 'к', printable: true },
+        { code: 'KeyT', en: 't', ru: 'е', printable: true },
+        { code: 'KeyY', en: 'y', ru: 'н', printable: true },
+        { code: 'KeyU', en: 'u', ru: 'г', printable: true },
+        { code: 'KeyI', en: 'i', ru: 'ш', printable: true },
+        { code: 'KeyO', en: 'o', ru: 'щ', printable: true },
+        { code: 'KeyP', en: 'p', ru: 'з', printable: true },
+        { code: 'BracketLeft', en: '[', ru: 'х', printable: true },
+        { code: 'BracketRight', en: ']', ru: 'ъ', printable: true },
+        { code: 'Backslash', en: '\\', ru: '\\', printable: true },
+        { code: 'Delete', en: 'DEL', ru: 'DEL', printable: false },
     ],
 
     [
-        { code: 20, en: 'Caps Lock', ru: 'Caps Lock', printable: false },
-        { code: 65, en: 'a', ru: 'ф', printable: true },
-        { code: 83, en: 's', ru: 'ы', printable: true },
-        { code: 68, en: 'd', ru: 'в', printable: true },
-        { code: 70, en: 'f', ru: 'а', printable: true },
-        { code: 71, en: 'g', ru: 'п', printable: true },
-        { code: 72, en: 'h', ru: 'р', printable: true },
-        { code: 74, en: 'j', ru: 'о', printable: true },
-        { code: 75, en: 'k', ru: 'л', printable: true },
-        { code: 76, en: 'l', ru: 'д', printable: true },
-        { code: 186, en: ';', ru: 'ж', printable: true },
-        { code: 222, en: '\'', ru: 'э', printable: true },
-        { code: 13, en: 'Enter', ru: 'Enter', printable: false },
+        { code: 'CapsLock', en: 'Caps Lock', ru: 'Caps Lock', printable: false },
+        { code: 'KeyA', en: 'a', ru: 'ф', printable: true },
+        { code: 'KeyS', en: 's', ru: 'ы', printable: true },
+        { code: 'KeyD', en: 'd', ru: 'в', printable: true },
+        { code: 'KeyF', en: 'f', ru: 'а', printable: true },
+        { code: 'KeyG', en: 'g', ru: 'п', printable: true },
+        { code: 'KeyH', en: 'h', ru: 'р', printable: true },
+        { code: 'KeyJ', en: 'j', ru: 'о', printable: true },
+        { code: 'KeyK', en: 'k', ru: 'л', printable: true },
+        { code: 'KeyL', en: 'l', ru: 'д', printable: true },
+        { code: 'Semicolon', en: ';', ru: 'ж', printable: true },
+        { code: 'Quote', en: '\'', ru: 'э', printable: true },
+        { code: 'Enter', en: 'Enter', ru: 'Enter', printable: false },
     ],
 
     [
-        { code: 16, en: 'Shift', ru: 'Shift', printable: false },
-        { code: 226, en: '\\', ru: '\\', printable: true },
-        { code: 90, en: 'z', ru: 'я', printable: true },
-        { code: 88, en: 'c', ru: 'с', printable: true },
-        { code: 67, en: 'v', ru: 'м', printable: true },
-        { code: 86, en: 'b', ru: 'и', printable: true },
-        { code: 66, en: 'n', ru: 'т', printable: true },
-        { code: 78, en: 'm', ru: 'ь', printable: true },
-        { code: 77, en: ',', ru: 'б', printable: true },
-        { code: 188, en: '.', ru: 'ю', printable: true },
-        { code: 190, en: '\/', ru: '.', printable: true },
-        { code: 191, en: '\'', ru: '.', printable: true },
-        { code: 38, en: '▲', ru: '▲', printable: true },
-        { code: 16, en: 'Shift', ru: 'Shift', printable: false },
+        { code: 'ShiftLeft', en: 'Shift', ru: 'Shift', printable: false },
+        { code: 'NoCode', en: '\\', ru: '\\', printable: true },
+        { code: 'KeyZ', en: 'z', ru: 'я', printable: true },
+        { code: 'KeyX', en: 'x', ru: 'ч', printable: true },
+        { code: 'KeyC', en: 'c', ru: 'с', printable: true },
+        { code: 'KeyV', en: 'v', ru: 'м', printable: true },
+        { code: 'KeyB', en: 'b', ru: 'и', printable: true },
+        { code: 'KeyN', en: 'n', ru: 'т', printable: true },
+        { code: 'KeyM', en: 'm', ru: 'ь', printable: true },
+        { code: 'Comma', en: ',', ru: 'б', printable: true },
+        { code: 'Period', en: '.', ru: 'ю', printable: true },
+        { code: 'Slash', en: '\/', ru: '.', printable: true },
+        { code: 'ArrowUp', en: '▲', ru: '▲', printable: false },
+        { code: 'ShiftRight', en: 'Shift', ru: 'Shift', printable: false },
     ],
 
     [
-        { code: 17, en: 'Ctrl', ru: 'Ctrl', printable: false },
-        { code: 91, en: 'Win', ru: 'Win', printable: false },
-        { code: 18, en: 'Alt', ru: 'Alt', printable: false },
-        { code: 32, en: ' ', ru: ' ', printable: true },
-        { code: 18, en: 'Alt', ru: 'Alt', printable: false },
-        { code: 37, en: '◄', ru: '◄', printable: true },
-        { code: 40, en: '▼', ru: '▼', printable: true },
-        { code: 39, en: '►', ru: '►', printable: true },
-        { code: 17, en: 'Ctrl', ru: 'Ctrl', printable: false }
+        { code: 'ControlLeft', en: 'Ctrl', ru: 'Ctrl', printable: false },
+        { code: 'MetaLeft', en: 'Win', ru: 'Win', printable: false },
+        { code: 'AltLeft', en: 'Alt', ru: 'Alt', printable: false },
+        { code: 'Space', en: ' ', ru: ' ', printable: true },
+        { code: 'AltRight', en: 'Alt', ru: 'Alt', printable: false },
+        { code: 'ArrowLeft', en: '◄', ru: '◄', printable: false },
+        { code: 'ArrowDown', en: '▼', ru: '▼', printable: false },
+        { code: 'ArrowRight', en: '►', ru: '►', printable: false },
+        { code: 'ControlRight', en: 'Ctrl', ru: 'Ctrl', printable: false }
     ],
 ];
 
@@ -145,79 +145,56 @@ let lang = 'en';
 fillKeyboard();
 
 function changeLanguage() {
-
     lang = (lang === 'ru') ? 'en' : 'ru';
-    //console.log(lang);
     fillKeyboard();
 }
 
-
 function fillKeyboard() {
-
     for (let i = 0; i < keyboard.children.length; i++) {
         for (let j = 0; j < keyboard.children[i].children.length; j++) {
+            let currentButton = keyboard.children[i].children[j];
+            keyboard.children[i].children[j].dataset.code = buttons[i][j].code;
+            keyboard.children[i].children[j].dataset.en = buttons[i][j].en;
+            keyboard.children[i].children[j].dataset.ru = buttons[i][j].ru;
+            keyboard.children[i].children[j].dataset.printable = buttons[i][j].printable;
             keyboard.children[i].children[j].textContent = buttons[i][j][lang];
         }
     }
 }
 
-
-
 const body = document.querySelector('body')
 
 body.addEventListener('keydown', (event) => {
-    buttons.forEach((i, indexI) => {
-        i.forEach((j, indexJ) => {
-            if (j.code === event.keyCode) {
-                keyboard.children[indexI].children[indexJ].classList.add('pressed');
-                printLetter(j[lang]);
-            }
-        })
-    })
+    let currentElement = document.querySelector(`[data-code="${event.code}"]`);
 
+    currentElement.classList.add('pressed');
+    printLetter(currentElement);
 })
 
 body.addEventListener('keyup', (event) => {
-    buttons.forEach((i, indexI) => {
-        i.forEach((j, indexJ) => {
-            if (j.code === event.keyCode) {
-                keyboard.children[indexI].children[indexJ].classList.remove('pressed');
-            }
-        })
-    })
+    document.querySelector(`[data-code="${event.code}"]`).classList.remove('pressed');
 })
-
 
 keyboard.addEventListener('click', event => {
-    printLetter(event.target.textContent);
+    if (event.target.classList.contains('key')) {
+        printLetter(event.target);
+    }
 })
 
-function printLetter(textContent) {
-    buttons.forEach((i) => {
-        i.forEach((j) => {
-            if (j.en === textContent || j.ru === textContent) {
-                if (j.printable) {
-                    textarea.textContent = textarea.textContent + textContent;
-                }
-            }
-        })
-    })
+function printLetter(currentElement) {
+    if (currentElement.dataset.printable === 'false') {
+        return;
+    }
+
+    let letter = (lang === 'en') ? currentElement.dataset.en : currentElement.dataset.ru;
+
+    textarea.textContent = textarea.textContent + letter;
 }
 
 let firstKey;
 
-let lastKey;
-
 body.addEventListener('keydown', (event) => {
     event.preventDefault();
-    if (lastKey === event.keyCode) {
-        //console.log(event.keyCode);
-        return;
-    }
-
-    lastKey = event.keyCode;
-
-
 
     if ((firstKey === 16 && event.keyCode === 18) || (firstKey === 18 && event.keyCode === 16)) {
         changeLanguage();
@@ -226,14 +203,11 @@ body.addEventListener('keydown', (event) => {
     }
 
     if (event.keyCode === 16 || event.keyCode === 18) {
-        //console.log(event.keyCode);
         firstKey = event.keyCode;
-        //console.log(firstKey);
     }
 })
 
 body.addEventListener('keyup', (event) => {
-
     if (event.keyCode === 16 || event.keyCode === 18) {
         firstKey = undefined;
     }
